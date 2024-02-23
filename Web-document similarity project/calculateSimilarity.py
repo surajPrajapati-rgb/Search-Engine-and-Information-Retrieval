36d86c784ae6075c66ccf0601a8267b6b51dd902
Suraj Prajapati <117726824+surajPrajapati-rgb@users.noreply.github.com>
2024-02-23 21:12:07 +0530

    Add files via upload

---
+++ Web-document similarity project/calculateSimilarity.py
@@ -0,0 +1,78 @@
+import sys
+import extract_webpage as ew
+
+if len(sys.argv) != 3:
+    print("Run code as simhash.py args1 args2 as ")
+    sys.exit(1)
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+punctuation = ['.', ',', ':', ';',  '/', '\\', '!', '?', '(',')', '[', ']', '{', '}', '|', '&', '^', '%', '<', '>', '~', '#']
+def remove_panctuation(original):
+    cleaned_data = ""
+    for i in original:
+        if i not in punctuation:
+            cleaned_data +=i
+    return cleaned_data
+
+def word_frequency(content):
+    data = content.split()
+    word_fre_dict = {}
+    for word in data:
+        if word not in word_fre_dict:
+            word_fre_dict[word] = 1
+        else:
+            word_fre_dict[word] += 1
+    return word_fre_dict
+
+def computeHashValue(word_dict):
+    p = 53
+    m = 2**64
+    words_with_hash = {}
+    for word in word_dict:
+        hash = 0
+        n=len(word)
+        for j in range(n):
+            ascii_value = ord(word[j])
+            hash += ascii_value*(p**j)  # s[n-1].p^(n-1)
+        hash = hash % m
+        binary_string = bin(hash)[2:]
+        padded_binary_string = binary_string.zfill(64)
+        words_with_hash[word] = (word_dict[word],padded_binary_string)
+    return words_with_hash
+
+def computeSimHash(original_text):
+    content = remove_panctuation(original_text)
+    word_dict = word_frequency(content)
+    hash64value = computeHashValue(word_dict)
+    weight_sum_vector = []
+    for i in range(64):
+        weight = 0
+        for j in hash64value:
+            if hash64value[j][1][i] == "1":
+                weight += hash64value[j][0]
+            else:
+                weight -= hash64value[j][0]
+        weight_sum_vector.append(weight)
+    # print(weight_sum_vector)
+    fingerprint_vector = [1 if weight > 0 else 0 for weight in weight_sum_vector]
+    return fingerprint_vector
+
+def compareSimhashes(simhash1,simhash2):
+    commonBits = 0
+    for i in range(64):
+        if simhash1[i] == simhash2[i]:
+            commonBits+=1
+
+    return f"The Common bits in simhashes is : {commonBits}"
+
+
+if __name__ == "__main__":
+    textcontent1 = ew.get_text_from_web(arg1)
+    simhash1 = computeSimHash(textcontent1)
+    textcontent2 = ew.get_text_from_web(arg2)
+    simhash2 = computeSimHash(textcontent2)
+    print(compareSimhashes(simhash1,simhash2))
+
+    
---
+++ Web-document similarity project/extract_webpage.py
@@ -0,0 +1,106 @@
+import requests
+
+def fetch_html_content(url):
+    try:
+        response = requests.get(url)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching content: {e}")
+        return None
+    
+def extract_title(html_content):
+    start = html_content.find("<title")
+    end = html_content.find("</title")
+    return html_content[start+7:end]
+
+def extract_body(html_content):
+    start = html_content.find("<body")
+    end = html_content.find("</body")
+    return html_content[start:end]
+
+def extract_main_content(html_content):
+    is_tag = False
+    is_script = False
+    is_style = False
+    is_span = False
+    text_content = ""
+
+    for i in range(len(html_content)):
+
+        if html_content[i] == '<':
+            is_tag = True
+            if html_content[i:i+6].lower() == '<style':
+                is_style = True
+            elif html_content[i] == '<' and html_content[i:i+7].lower() == '</style':
+                is_style = False
+            if html_content[i:i+7].lower() == '<script':
+                is_script = True
+            elif html_content[i] == '<' and html_content[i:i+8].lower() == '</script':
+                is_script = False
+            if html_content[i:i+5].lower() == '<span':
+                is_span = True
+            elif html_content[i] == '<' and html_content[i:i+6].lower() == '</span':
+                is_span = False
+
+        elif html_content[i] == '>':
+            is_tag = False
+            continue
+
+        if not is_tag and not is_script and not is_style and not is_span:
+            text_content += html_content[i]
+    return text_content
+
+def remove_extra_spaces(text_content):
+    cleaned_data=" "
+    for i in range(len(text_content)):
+        if (text_content[i] in ["\n","\r","\t"] and cleaned_data[-1] in ["\n","\r","\t"]):
+            continue
+        cleaned_data += text_content[i]
+    return cleaned_data
+
+def checkLinks(tag):
+    start = tag.find('<a href')
+    link = tag.find('http',start+7,start+21)
+    isLink =  link != -1
+    if start != -1 and isLink :
+        first_quatation = tag.find('"',start+6)
+        second_quatation = tag.find('"',first_quatation+1 )
+        print(tag[first_quatation +1 :second_quatation])
+
+def extract_link(html_content):
+    tag = ""
+    i = 0
+    while i < len(html_content):
+        if html_content[i] == '<':
+            isTagOpen = True
+
+        elif html_content[i] == '>':
+            isTagOpen = False
+
+        if isTagOpen :
+            tag += html_content[i]
+
+        elif not isTagOpen :
+            checkLinks(tag)
+            tag = ""
+        i = i+1
+
+def get_text_from_web(url):
+    html_content = fetch_html_content(url)
+    body_content = extract_body(html_content)
+    text_content = extract_main_content(body_content)
+    textdata = extract_title(html_content) +"\n"+ remove_extra_spaces(text_content)
+    processed_text = ""
+    i = 0
+    while i < len(textdata)-1:
+        if textdata[i] == "&" and textdata[i+1] == "#":
+            t = textdata.find(";",i)
+            i = t+1
+            continue 
+        processed_text += textdata[i]
+        i += 1
+    return processed_text 
+
+# if __name__ == "__main__":
+#     print(get_text_from_web("https://sitare.org/univ/"))
+
